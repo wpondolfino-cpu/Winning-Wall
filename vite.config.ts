f055d2e068d43a9d35aa964eb25d3ea2ee9b52d8
wpondolfino-cpu <wpondolfino@attleborops.net>
2026-05-13 05:48:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 })
