c05c3228dd9f791e63b15933941c92ebf292034e
wpondolfino-cpu <wpondolfino@attleborops.net>
2026-06-26 10:11:33 -0400

    Update ClassClash.tsx

--- src/components/challenges/ClassClash.tsx
+++ src/components/challenges/ClassClash.tsx
@@ -1,6 +1,6 @@
 // src/components/ClassClash.tsx
 import { useState, useEffect } from "react";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 interface Props {
   currentUserId: string;
